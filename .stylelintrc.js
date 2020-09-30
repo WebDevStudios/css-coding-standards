6e10fe5a6533a8426b6d929dbc0d2508ce543bd7
Aubrey Portwood <code@aubreypwd.com>
2020-09-30 09:39:38 -0600

    Use only Stylelint
    This removes sass-lint and adds all the rules to .stylelintrc.js which comes in as main.
    
    The contents of .stylelintrc.js comes from https://github.com/WebDevStudios/wd_s/blob/feature/tailwind/.stylelintrc.js per @coreycollins

---
+++ .stylelintrc.js
@@ -0,0 +1,29 @@
+/**
+ * Stylelint Configuration
+ *
+ * To use add:
+ *
+ *    stylelint": {
+ *      "extends": "@webdevstudios/css-coding-standards"
+ *    }
+ *
+ * ...to the `package.json` of your project.
+ *
+ * @since 2.0.0
+ */
+
+module.exports = {
+	extends: [
+		'stylelint-config-wordpress'
+	],
+	rules: {
+		'at-rule-no-unknown': null,
+		'block-no-empty': null,
+		'font-family-no-duplicate-names': null,
+		'font-family-no-missing-generic-family-keyword': null,
+		'max-line-length': null,
+		'no-descending-specificity': null,
+		'no-duplicate-selectors': null,
+		'scss/at-rule-no-unknown': true,
+	},
+};
