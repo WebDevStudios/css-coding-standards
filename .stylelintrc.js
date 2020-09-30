ee932ba80a90f46333faad1b2dd90d70f4e59ca9
Aubrey Portwood <code@aubreypwd.com>
2020-09-30 14:07:09 -0600

    Port ignores from SASS

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -14,16 +14,33 @@
 
 module.exports = {
 	extends: [
-		'stylelint-config-wordpress/scss' // This extends stylelint-config-wordpress as well.
+		'stylelint-config-wordpress/scss', // This extends stylelint-config-wordpress as well.
 	],
 	rules: {
 		...require( './src/wd_s.js' ),
 		...require( './src/sass-lint-ports.js' ),
 	},
-	"ignoreFiles": [
-		'!*.scss', // Anything but .scss files.
-		'!*.css', // Anything bug .css files.
+	ignoreFiles: [
+
+		// Anything but SCSS and CSS files.
+		'!*.scss',
+		'!*.css',
+
+		// Node and Composer.
 		'/node_modules/',
 		'/vendor/',
+
+		// wd_s ignore list.
+		'assets/**/_colors.scss',
+		'assets/**/_gravity-forms.scss',
+		'assets/**/_scaffolding.scss',
+		'assets/**/style.scss',
+		'assets/bower_components/**/*.scss',
+		'assets/sass/vendor/**/*.scss',
+		'bourbon-neat/app/**/*.scss',
+		'bourbon/app/**/*.scss',
+		'node_modules/**/*.scss',
+		'sass/tests/**/*.scss',
+		'sass/vendor/**/*.scss',
 	],
 };
